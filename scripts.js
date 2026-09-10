1ce1cd64dc26306a55194b9140ddf04ebd7e238a
adssenacmga <adssenacmga@gmail.com>
2026-09-10 16:47:14 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -34,13 +34,11 @@
             function enviarEmail() {
             const nome = document.getElementById('nome').value;
             const mensagem = document.getElementById('mensagem').value;
-            
-            // Substitua pelo seu endereço do Gmail
             const destinatario = "juliana.ramos@pr.senac.br";
             const email = document.getElementById('email').value;
-            const email = document.getElementById('fone').value;
-            const assunto = encodeURIComponent("Contact from the website of " + nome);
-            const corpo = encodeURIComponent("Name: " + nome + "\nEmail: " + email  + "\n\nMessage:\n" + mensagem);
+            const fone = document.getElementById('fone').value;
+            const assunto = encodeURIComponent("Contato do site feito por " + nome);
+            const corpo = encodeURIComponent("Nome: " + nome + "\nEmail: " + email  + "\nTelefone: " + fone + "\n\nMensagem:\n" + mensagem);
             
             // Abre o cliente de e-mail padrão ou o Gmail na Web
             window.location.href = `mailto:${destinatario}?subject=${assunto}&body=${corpo}`;
